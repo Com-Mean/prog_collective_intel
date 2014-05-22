2993902c131414fb38eb487261b0e1d07671d758
Com-Mean <out_1018@outlook.com>
2014-05-23 07:19:00 +0800

    update

--- search_engine.py
+++ search_engine.py
@@ -12,6 +12,7 @@ from urlparse import urljoin
 import sqlite3 as sqlite
 
 
+Ignore_words = set(['the', 'of', 'to', 'and', 'a', 'in', 'is', 'it'])
 class crawler:
     def __init__(self, dbname):
         self.con = sqlite.connect(dbname)
@@ -84,3 +85,47 @@ class crawler:
 
         pass
 
+class Searcher:
+    def __init__(self, dbname):
+        self.con = sqlite.connect(dbname)
+
+    def __del__(self):
+        self.con.close()
+
+    def get_scored_list(self, rows, word_ids):
+        total_scores = dict([(row[0], 0) for row in rows])
+
+        #TODO evaluate function
+        weights = []
+        for (weight, scores) in weights:
+            for url in total_scores.keys():
+                total_scores[url] += weight * scores[url]
+        
+        return total_scores
+
+    def get_url_name(self, id):
+        return self.con.execute(
+                "select url from urllist where rowid=%d"%id).fetchone()[0]
+
+    def query(self, q):
+        rows, word_ids = self.get_match_rows(q)
+        scores = self.get_scored_list(rows, word_ids)
+
+        ranked_scores = sorted([(score, url) for (url, score) in scores.items()], \
+                reverse = True)
+        for (score, url_id) in ranked_scores[0:10]:
+            print('%f\t%s'%(score, self.get_url_name(url_id)))
+
+
+    def get_match_rows(self, q):
+        field_list = 'w0.rulid'
+        table_list = ''
+        clause_list = ''
+        word_ids = []
+
+        words = q.split(' ')
+        table_num = 0
+
+        if word in words:
+            word_row = self.con.execute(
+                    "select rowid from wordlist where word='%s'"%word).fetchone()
