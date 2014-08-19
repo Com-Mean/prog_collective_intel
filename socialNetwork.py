234bdea8c06bd090bf8f0c7ca951dcbf1f21791b
Com-Mean <out_1018@outlook.com>
2014-08-20 02:12:26 +0800

    chapter 6

---
+++ socialNetwork.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# File Name: socialNetwork.py
+# Author: lpqiu
+# mail: qlp_1018@126.com
+# Created Time: 2014年08月19日 星期二 22时57分24秒
+#########################################################################
+
+import math
+
+people = ['Charlie', 'Augustus', 'Veruca', 'Violet', 'Mike','Joe', 'Willy', 'Miranda']
+
+links = [('Augustus', 'Willy'),
+         ('Mike', 'Joe'),
+         ('Miranda', 'Mike'),
+         ('Violet', 'Augustus'),
+         ('Miranda', 'Willy'),
+         ('Charlie', 'Mike'),
+         ('Veruca', 'Joe'),
+         ('Miranda', 'Augustus'),
+         ('Willy', 'Augustus'),
+         ('Joe', 'Charlie'),
+         ('Veruca', 'Augustus'),
+         ('Miranda', 'Joe')
+         ]
