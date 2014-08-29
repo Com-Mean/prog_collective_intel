8e55f5b38f52d3dbca34234fae197d57202d6ceb
Com-Mean <out_1018@outlook.com>
2014-08-29 15:38:43 +0800

    机器学习实战  Please enter the commit message for your changes. Lines starting

---
+++ treePlotter.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#########################################################################
+# File Name: treePlotter.py
+# Author: lpqiu
+# mail: qlp_1018@126.com
+# Created Time: 2014年08月28日 星期四 20时42分47秒
+#########################################################################
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle = "sawtooth", fc="0.8")
+leafNode = dict(boxstyle = 'round4', fc = '0.8')
+arrow_args = dict(arrowstyle = "<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, \
+            xycoords='axes fraction', \
+            xytext = centerPt, textcoords='axes fraction', \
+            va = 'center', ha='center', bbox=nodeType, arrowprops = arrow_args)
+
+    '''
+def createPlot():
+    fig = plt.figure(1, facecolor = 'white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111,frameon=False)
+    print(createPlot.ax1)
+    plotNode(U'DecisionNode', (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode(U'LeafNode', (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+    '''
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else: numLeafs += 1
+
+    return numLeafs
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else: thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+def plotMidText(cntrpt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrpt[0])/2.0 + cntrpt[0]
+    yMid = (parentPt[0] - cntrpt[0])/2.0 + cntrpt[0]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    maxDepth = getTreeDepth(myTree)
+    firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) /2.0 /plotTree.totalW, \
+            plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), \
+                    cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor = 'white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW; plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+if __name__ == "__main__":
+    createPlot()
